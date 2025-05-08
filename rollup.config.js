c35b6e79794ae975bde78569f67e287cefe4ec6e
fa7ll7en <brandonlewis@anvilrockroad.com>
2025-05-08 10:48:48 -0700

    Update Rollup configuration with minification and banners

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,49 @@
-export default {
-  input: 'src/index.js',
-  output: [
-    {
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json' assert { type: 'json' };
+
+// Banner for the builds
+const banner = `/**
+ * ${pkg.name} v${pkg.version}
+ * ${pkg.description}
+ * ${pkg.homepage}
+ * 
+ * @license ${pkg.license}
+ * @copyright ${new Date().getFullYear()} ${pkg.author}
+ */
+`;
+
+export default [
+  // UMD build for browsers (production, minified)
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/gnss.min.js',
+      format: 'umd',
+      name: 'GnssModule',
+      banner,
+      sourcemap: true,
+      plugins: [terser()]
+    }
+  },
+  // UMD build for browsers (development, non-minified)
+  {
+    input: 'src/index.js',
+    output: {
       file: 'dist/gnss.js',
       format: 'umd',
       name: 'GnssModule',
-      sourcemap: true
-    },
-    {
-      file: 'dist/gnss.esm.js',
-      format: 'esm',
+      banner,
       sourcemap: true
     }
-  ]
-};
+  },
+  // ESM build for modern environments
+  {
+    input: 'src/index.js',
+    output: {
+      file: pkg.module,
+      format: 'esm',
+      banner,
+      sourcemap: true
+    }
+  }
+];
